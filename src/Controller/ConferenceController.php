1277e4e8b858c4eff5f1c0b7e2db5336f24024f4
Andrei Nadezhkin <andreikoman@gmail.com>
2024-02-22 15:36:28 +0300

    home page

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
+    {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
+//        dump($request);
+
+        return new Response(<<<EOF
+            <html lang="en">
+                <body>
+                    $greet
+                    <img src="/images/image.jpg"  alt="test"/>
+                </body>
+           </html>
+EOF
+        );
+    }
+}
